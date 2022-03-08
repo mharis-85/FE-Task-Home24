1c0af2438a44d61a8e04dbeccb676839b01d1468
mharis0085 <mharis0085@gmail.com>
2022-03-08 23:47:39 +0500

    Routes structure updated

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,8 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import HomePage from './pages/Home';
+//import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Routes from './routes/routes';
 
 const App: React.FC = () => {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/:kategorie" element={<HomePage />} />
-                <Route path="/:kategorie/*" element={<HomePage />} />
-            </Routes>
-        </BrowserRouter>
-    );
+    return <Routes />;
 };
 export default App;
